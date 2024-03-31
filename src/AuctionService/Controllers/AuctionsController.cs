12863a327e1512426c2328bc43bc3e64342062d0
Onur Ucar <hello@ucaronur.com>
2024-03-31 01:02:29 -0400

    Fixing the 502

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -19,11 +19,8 @@ public class AuctionsController : ControllerBase
     private readonly IMapper _mapper;
     private readonly IPublishEndpoint _publishEndpoint;
 
-    public AuctionsController(
-        AuctionDbContext context,
-        IMapper mapper,
-        IPublishEndpoint publishEndpoint
-    )
+    public AuctionsController(AuctionDbContext context, IMapper mapper,
+        IPublishEndpoint publishEndpoint)
     {
         _context = context;
         _mapper = mapper;
@@ -35,15 +32,11 @@ public class AuctionsController : ControllerBase
     {
         var query = _context.Auctions.OrderBy(x => x.Item.Make).AsQueryable();
 
-        // Filter auctions by date if provided
         if (!string.IsNullOrEmpty(date))
         {
-            query = query.Where(
-                x => x.UpdatedAt.CompareTo(DateTime.Parse(date).ToUniversalTime()) > 0
-            );
+            query = query.Where(x => x.UpdatedAt.CompareTo(DateTime.Parse(date).ToUniversalTime()) > 0);
         }
 
-        // Project the query to AuctionDto
         return await query.ProjectTo<AuctionDto>(_mapper.ConfigurationProvider).ToListAsync();
     }
 
@@ -54,8 +47,7 @@ public class AuctionsController : ControllerBase
             .Include(x => x.Item)
             .FirstOrDefaultAsync(x => x.Id == id);
 
-        if (auction == null)
-            return NotFound();
+        if (auction == null) return NotFound();
 
         return _mapper.Map<AuctionDto>(auction);
     }
@@ -68,53 +60,42 @@ public class AuctionsController : ControllerBase
 
         auction.Seller = User.Identity.Name;
 
-        auction.Seller = "test";
-
         _context.Auctions.Add(auction);
 
-        // Publish the auction to the message broker
-        // If the service bus is up - the message will be sent
-        // If the service bus is down - the message will be published to the outbox, and then the transaction will fail.
         var newAuction = _mapper.Map<AuctionDto>(auction);
+
         await _publishEndpoint.Publish(_mapper.Map<AuctionCreated>(newAuction));
 
         var result = await _context.SaveChangesAsync() > 0;
 
-        if (!result)
-            return BadRequest("ERROR: Could not save changes to the DB!");
+        if (!result) return BadRequest("Could not save changes to the DB");
 
-        return CreatedAtAction(nameof(GetAuctionById), new { auction.Id }, newAuction);
+        return CreatedAtAction(nameof(GetAuctionById),
+            new { auction.Id }, newAuction);
     }
 
     [Authorize]
     [HttpPut("{id}")]
     public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAuctionDto)
     {
-        var auction = await _context.Auctions
-            .Include(x => x.Item)
+        var auction = await _context.Auctions.Include(x => x.Item)
             .FirstOrDefaultAsync(x => x.Id == id);
 
-        if (auction == null)
-            return NotFound();
+        if (auction == null) return NotFound();
 
-        if (auction.Seller != User.Identity.Name)
-            return Forbid();
+        if (auction.Seller != User.Identity.Name) return Forbid();
 
-        // Update auction properties if they are not null
-        // If they are null, keep the original value
         auction.Item.Make = updateAuctionDto.Make ?? auction.Item.Make;
         auction.Item.Model = updateAuctionDto.Model ?? auction.Item.Model;
         auction.Item.Color = updateAuctionDto.Color ?? auction.Item.Color;
         auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
         auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
 
-        // Publish the updated auction to the message broker
         await _publishEndpoint.Publish(_mapper.Map<AuctionUpdated>(auction));
 
         var result = await _context.SaveChangesAsync() > 0;
 
-        if (result)
-            return Ok();
+        if (result) return Ok();
 
         return BadRequest("Problem saving changes");
     }
@@ -125,22 +106,19 @@ public class AuctionsController : ControllerBase
     {
         var auction = await _context.Auctions.FindAsync(id);
 
-        if (auction == null)
-            return NotFound();
+        if (auction == null) return NotFound();
 
-        if (auction.Seller != User.Identity.Name)
-            return Forbid();
+        if (auction.Seller != User.Identity.Name) return Forbid();
 
         _context.Auctions.Remove(auction);
 
-        // Publish the deleted auction to the message broker
         await _publishEndpoint.Publish<AuctionDeleted>(new { Id = auction.Id.ToString() });
 
         var result = await _context.SaveChangesAsync() > 0;
 
-        if (!result)
-            return BadRequest("Could not update DB");
+        if (!result) return BadRequest("Could not update DB");
 
         return Ok();
     }
+
 }
